fd8f62596898f495cc7fff0e922d1c48ab8e3b19
Ehab Elshahat <61202200+Ehab-Elshahat@users.noreply.github.com>
2022-05-10 16:17:45 +0200

    Escape evant And overlay

--- js/code.js
+++ js/code.js
@@ -1,12 +1,25 @@
 // Open Close Meue In small screen
 let showMenue = document.querySelector(".show");
 let menue = document.querySelector(".navbar");
+let overlay = document.querySelector(".overlay");
 showMenue.addEventListener("click", () => {
   menue.classList.toggle("open");
+  overlay.classList.toggle("open");
 
   showMenue.firstElementChild.classList.toggle("fa-xmark");
   showMenue.firstElementChild.classList.toggle("fa-bars");
 });
+
+window.onkeyup = function (e) {
+  if (menue.classList.contains("open")) {
+    if (e.code === "Escape") {
+      menue.classList.remove("open");
+      overlay.classList.remove("open");
+      showMenue.firstElementChild.classList.toggle("fa-xmark");
+      showMenue.firstElementChild.classList.toggle("fa-bars");
+    }
+  }
+};
 
 // Open Product details In new Page
 let allProducts = document.querySelectorAll(".products .product-box");
